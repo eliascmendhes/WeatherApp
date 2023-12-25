701e240875418e924568b98176e69670bf5d57cf
ELIAS CAMILO MENDES <ELIASCAMILO70@OUTLOOK.COM>
2023-12-25 10:19:38 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Elias Camilo on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
